e99295fc1bc0c2d5fb72e89f08df368894f0e4ef
Jormungandr72 <patrickjmcgranahan@gmail.com>
2026-03-31 08:52:03 -0500

    Continued work on map generation script

--- Scripts/MapGen.gd
+++ Scripts/MapGen.gd
@@ -1,9 +1,10 @@
 class MapGen extends Node3D:
 	'
-	Script to generate a miscellaneous structure of tile objects.
+	Contains methods to generate a miscellaneous structure of tile objects.
 	'
 	@onready var _map = get_node("res://Scenes/Map")
-	@onready var _spawn_coordinates = Vector3i(0, 0, 0)
+	static var _spawn_coordinates: Vector3i = Vector3i(0, 0, 0)
+	static var min_dist: int = 5 # Minimum distance between objective tiles
 	
 	# Methods
 	func _ready():
@@ -13,14 +14,22 @@ class MapGen extends Node3D:
 		'
 		Generates a completely random map of x * y size.
 		'
-		var curr_tile: Material
 		for i in x:
 			for j in y:
 				_map.set_cell_item(Vector3i(i,j,-1), randi_range(0, 4))
 		return
 
-	func procedural_gen():
+	func procedural_gen(rounds:int = 20):
 		'
 		Procedurally generate map based on a set of rules.
+		
+		Args:
+			rounds (int): number of times to run through the generation loop
 		'
-		pass
+		var curr_dist: int = None # Tracks distance between objective tiles
+		
+		for i in rounds:
+			match randi_range(0,4): # From 0 to maximum number of tiles in meshlibrary
+				0: # Objective tile
+					# No objective tile within x spaces of eachother
+					# No objective tile within 10 spaces of spawn
